0c8795bc34699aafe58bbb4bc62bf90616602924
Chloe-H <Chloe-H@users.noreply.github.com>
2023-07-07 11:38:55 -0500

    [nvim] Add vim-fugitive

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,6 +3,7 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug 'junegunn/vim-plug' -- to get Vim help for vim-plug
 Plug 'tpope/vim-surround'
+Plug 'tpope/vim-fugitive'
 
 -- TODO: jiangmiao/auto-pairs?
 
