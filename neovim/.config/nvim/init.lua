79abbc61c5fbd61927ae99cdc97cc20e6e1da888
Chloe-H <Chloe-H@users.noreply.github.com>
2023-07-10 20:56:50 -0500

    [nvim] Configure OmniSharp, csharp_ls

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -7,7 +7,10 @@ Plug 'junegunn/vim-plug'
 
 -- Niceties
 Plug 'tpope/vim-surround'
+
+-- LSP stuff
 Plug 'neovim/nvim-lspconfig'
+Plug 'Decodetalkers/csharpls-extended-lsp.nvim'
 
 -- Git
 Plug 'tpope/vim-fugitive'
@@ -59,6 +62,11 @@ vim.o.softtabstop = 4
 -- Number of spaces to use for each step of (auto)indent
 vim.o.shiftwidth = 4
 
+vim.diagnostic.config ({
+    virtual_text = false,
+    signs = true,
+    underline = true,
+})
 
 
 -- Plugin settings
@@ -73,34 +81,192 @@ vim.api.nvim_set_keymap(
 
 
 -- Plugin settings: lspconfig
+local lspconfig = require('lspconfig')
+
+local on_attach_apply_universal_lsp_configs = function(client, bufnr)
+    -- Enable completion triggered by <C-x><C-o>
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    --[[
+        Buffer local mappings
+        See `:help vim.lsp.*` for documentation on the functions below
+    --]]
+    local bufopts = {
+        buffer = bufnr,
+        remap = false,
+    }
+
+    vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', '<Leader>sd', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<Leader>sh', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<Leader>fu', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
+
+    vim.keymap.set('n', '<Leader>ed', vim.diagnostic.open_float, bufopts)
+    vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, bufopts)
+    vim.keymap.set('n', ']e', vim.diagnostic.goto_next, bufopts)
+end
+
+-- Useful things for configuring C# language servers
+
+-- Types of files the C# LSPs can be started in
+local csharp_filetypes = { 'solution', 'xml', 'cs', 'vb' }
+
+-- Gets the root directory
+local csharp_root_dir = function(startpath)
+    -- Source: https://github.com/neovim/nvim-lspconfig/issues/2612
+    return lspconfig.util.root_pattern('*.sln')(startpath)
+        or lspconfig.util.root_pattern('*.csproj')(startpath)
+        or lspconfig.util.root_pattern('*.fsproj')(startpath)
+        or lspconfig.util.root_pattern('.git')(startpath)
+end
+
+-- Gets the C# language server's source (i.e. an absolute path to a sln,
+-- csproj, or dir)
+local csharp_source = function()
+    local source_path
+    local file_extension = vim.fn.expand('%:e')
+
+    if file_extension == 'sln' or file_extension == 'csproj' then
+        -- Use the path to the open project or solution
+        source_path = vim.fn.expand('%:p')
+    else
+        -- Get the nearest ancestor dir containing a C# root-y file
+        source_path = csharp_root_dir(vim.fn.expand('%:p:h'))
+    end
+
+    return source_path
+end
+
+--[[
+    LSP settings: csharp-language-server
+    lang(s): C#
+--]]
+
+lspconfig['csharp_ls'].setup({
+    autostart = false,
+    filetypes = csharp_filetypes,
+    cmd = {
+        'csharp-ls',
+        '--solution',
+        csharp_source(),
+    },
+    root_dir = csharp_root_dir,
+    on_attach = on_attach_apply_universal_lsp_configs,
+    handlers = {
+        ['textDocument/definition'] = require('csharpls_extended').handler,
+        ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
+            -- Disable diagnostics because they're ALL WRONG
+            result.diagnostics = {}
+        end,
+    }
+})
 
 --[[
     LSP settings: OmniSharp-Roslyn
     lang(s): C#
+
     Source(s):
-      - https://aaronbos.dev/posts/csharp-dotnet-neovim
-      - https://alpha2phi.medium.com/neovim-for-beginners-lsp-part-1-b3a17ddbe611
+        - https://stackoverflow.com/a/76225760
 --]]
 
-local pid = vim.fn.getpid()
+local omnisharp_server_location = 'C:\\OmniSharp\\omnisharp-win-x64\\OmniSharp.exe'
 
-local omnisharp_bin = 'C:\\OmniSharp\\omnisharp-win-x64\\OmniSharp.exe'
-
-require'lspconfig'.omnisharp.setup {
-    cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) },
+lspconfig['omnisharp'].setup({
+    autostart = false,
+    filetypes = csharp_filetypes,
+    cmd = {
+        omnisharp_server_location,
+        '--languageserver',
+        '--hostPID',
+        tostring(vim.fn.getpid()),
+        '--source',
+        csharp_source(),
+    },
+    root_dir = csharp_root_dir,
     on_attach = function(client, bufnr)
-        -- Enable completion triggered by <C-x><C-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+        on_attach_apply_universal_lsp_configs(client, bufnr)
 
-        -- Buffer local mappings
-        -- See `:help vim.lsp.*` for documentation on the functions below
-        local bufopts = { buffer = bufnr }
-
-        vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', '<Leader>sd', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', '<Leader>sh', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<Leader>fu', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
-    end
-}
+        --[[
+            Temporary fix for a Roslyn issue in OmniSharp
+            Relevant:
+                - https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
+                - https://github.com/neovim/neovim/issues/21391
+        --]]
+        client.server_capabilities.semanticTokensProvider = {
+                range = true,
+                full = vim.empty_dict(),
+                legend = {
+                    tokenModifiers = { 'static_symbol' },
+                    tokenTypes = {
+                        'comment',
+                        'excluded_code',
+                        'identifier',
+                        'keyword',
+                        'keyword_control',
+                        'number',
+                        'operator',
+                        'operator_overloaded',
+                        'preprocessor_keyword',
+                        'string',
+                        'whitespace',
+                        'text',
+                        'static_symbol',
+                        'preprocessor_text',
+                        'punctuation',
+                        'string_verbatim',
+                        'string_escape_character',
+                        'class_name',
+                        'delegate_name',
+                        'enum_name',
+                        'interface_name',
+                        'module_name',
+                        'struct_name',
+                        'type_parameter_name',
+                        'field_name',
+                        'enum_member_name',
+                        'constant_name',
+                        'local_name',
+                        'parameter_name',
+                        'method_name',
+                        'extension_method_name',
+                        'property_name',
+                        'event_name',
+                        'namespace_name',
+                        'label_name',
+                        'xml_doc_comment_attribute_name',
+                        'xml_doc_comment_attribute_quotes',
+                        'xml_doc_comment_attribute_value',
+                        'xml_doc_comment_cdata_section',
+                        'xml_doc_comment_comment',
+                        'xml_doc_comment_delimiter',
+                        'xml_doc_comment_entity_reference',
+                        'xml_doc_comment_name',
+                        'xml_doc_comment_processing_instruction',
+                        'xml_doc_comment_text',
+                        'xml_literal_attribute_name',
+                        'xml_literal_attribute_quotes',
+                        'xml_literal_attribute_value',
+                        'xml_literal_cdata_section',
+                        'xml_literal_comment',
+                        'xml_literal_delimiter',
+                        'xml_literal_embedded_expression',
+                        'xml_literal_entity_reference',
+                        'xml_literal_name',
+                        'xml_literal_processing_instruction',
+                        'xml_literal_text',
+                        'regex_comment',
+                        'regex_character_class',
+                        'regex_anchor',
+                        'regex_quantifier',
+                        'regex_grouping',
+                        'regex_alternation',
+                        'regex_text',
+                        'regex_self_escaped_character',
+                        'regex_other_escape',
+                    },
+                },
+            }
+    end,
+})
