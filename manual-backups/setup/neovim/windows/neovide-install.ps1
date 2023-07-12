840dcb43550a9e7c15de768d11f6132285f4d87d
Chloe-H <Chloe-H@users.noreply.github.com>
2023-07-11 20:28:59 -0500

    [nvim] Add setup script for Windows
    Very basic, untested, but better than nothings.

---
+++ manual-backups/setup/neovim/windows/neovide-install.ps1
@@ -0,0 +1,26 @@
+# Alternate source for a lot of this: https://community.ops.io/aowendev/managing-packages-on-windows-with-scoop-411d
+
+# Get current execution policy, in case you want to reset it after the install
+Get-ExecutionPolicy -Scope CurrentUser
+
+# Optional: Needed to run a remote script the first time
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Install scoop
+irm get.scoop.sh | iex
+
+# Add extras bucket
+scoop install git
+scoop bucket add extras
+
+# Install neovim and neovide
+scoop install neovim
+scoop install vcredist2022
+scoop install neovide
+
+# Install vim-plug: https://github.com/junegunn/vim-plug#windows-powershell-1
+# fzf.vim dependencies:
+# scoop install perl # For `:Tags`, `:Helptags`
+
+# TODO: reset execution policy (depending on the machine)
+# Keeping in mind you need RemoteSigned to run scoop commands
