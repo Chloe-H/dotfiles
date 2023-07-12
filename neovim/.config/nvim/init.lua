2e7ceadd152bc82d060403d30ab53f8387f32883
Chloe-H <Chloe-H@users.noreply.github.com>
2023-07-12 10:53:46 -0500

    [nvim] Remove omnisharp_vscode
    Cursory tests suggest it wasn't doing better than OmniSharp-Roslyn.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -240,89 +240,6 @@ local csharp_source = function()
     return source_path
 end
 
-local on_attach_apply_omnisharp_workaround = function(client, bufnr)
-    --[[
-        Temporary fix for a Roslyn issue in OmniSharp
-        Relevant:
-            - https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
-            - https://github.com/neovim/neovim/issues/21391
-    --]]
-    client.server_capabilities.semanticTokensProvider = {
-        range = true,
-        full = vim.empty_dict(),
-        legend = {
-            tokenModifiers = { 'static_symbol' },
-            tokenTypes = {
-                'comment',
-                'excluded_code',
-                'identifier',
-                'keyword',
-                'keyword_control',
-                'number',
-                'operator',
-                'operator_overloaded',
-                'preprocessor_keyword',
-                'string',
-                'whitespace',
-                'text',
-                'static_symbol',
-                'preprocessor_text',
-                'punctuation',
-                'string_verbatim',
-                'string_escape_character',
-                'class_name',
-                'delegate_name',
-                'enum_name',
-                'interface_name',
-                'module_name',
-                'struct_name',
-                'type_parameter_name',
-                'field_name',
-                'enum_member_name',
-                'constant_name',
-                'local_name',
-                'parameter_name',
-                'method_name',
-                'extension_method_name',
-                'property_name',
-                'event_name',
-                'namespace_name',
-                'label_name',
-                'xml_doc_comment_attribute_name',
-                'xml_doc_comment_attribute_quotes',
-                'xml_doc_comment_attribute_value',
-                'xml_doc_comment_cdata_section',
-                'xml_doc_comment_comment',
-                'xml_doc_comment_delimiter',
-                'xml_doc_comment_entity_reference',
-                'xml_doc_comment_name',
-                'xml_doc_comment_processing_instruction',
-                'xml_doc_comment_text',
-                'xml_literal_attribute_name',
-                'xml_literal_attribute_quotes',
-                'xml_literal_attribute_value',
-                'xml_literal_cdata_section',
-                'xml_literal_comment',
-                'xml_literal_delimiter',
-                'xml_literal_embedded_expression',
-                'xml_literal_entity_reference',
-                'xml_literal_name',
-                'xml_literal_processing_instruction',
-                'xml_literal_text',
-                'regex_comment',
-                'regex_character_class',
-                'regex_anchor',
-                'regex_quantifier',
-                'regex_grouping',
-                'regex_alternation',
-                'regex_text',
-                'regex_self_escaped_character',
-                'regex_other_escape',
-            },
-        },
-    }
-end
-
 --[[
     LSP settings: csharp-language-server
     lang(s): C#
@@ -348,31 +265,6 @@ lspconfig['csharp_ls'].setup({
 })
 
 --[[
-    LSP settings: VS Code's OmniSharp installation
-    lang(s): C#
-
-    You'll need to make a copy of lspconfig's omnisharp config file and rename
-    it to match the key used here.
---]]
-lspconfig['omnisharp_vscode'].setup({
-    autostart = false,
-    filetypes = csharp_filetypes,
-    cmd = {
-        'C:\\Users\\chloe.h[...]\\.vscode\\extensions\\ms-dotnettools.csharp-1.26.0-win32-x64\\.omnisharp\\1.39.7\\OmniSharp.exe',
-        '--languageserver',
-        '--hostPID',
-        tostring(vim.fn.getpid()),
-        '--source',
-        csharp_source(),
-    },
-    root_dir = csharp_root_dir,
-    on_attach = function(client, bufnr)
-        on_attach_apply_universal_lsp_configs(client, bufnr)
-        on_attach_apply_omnisharp_workaround(client, bufnr)
-    end,
-})
-
---[[
     LSP settings: OmniSharp-Roslyn
     lang(s): C#
 
@@ -393,7 +285,87 @@ lspconfig['omnisharp'].setup({
     root_dir = csharp_root_dir,
     on_attach = function(client, bufnr)
         on_attach_apply_universal_lsp_configs(client, bufnr)
-        on_attach_apply_omnisharp_workaround(client, bufnr)
+
+        --[[
+            Temporary fix for a Roslyn issue in OmniSharp
+            Relevant:
+                - https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
+                - https://github.com/neovim/neovim/issues/21391
+        --]]
+        client.server_capabilities.semanticTokensProvider = {
+            range = true,
+            full = vim.empty_dict(),
+            legend = {
+                tokenModifiers = { 'static_symbol' },
+                tokenTypes = {
+                    'comment',
+                    'excluded_code',
+                    'identifier',
+                    'keyword',
+                    'keyword_control',
+                    'number',
+                    'operator',
+                    'operator_overloaded',
+                    'preprocessor_keyword',
+                    'string',
+                    'whitespace',
+                    'text',
+                    'static_symbol',
+                    'preprocessor_text',
+                    'punctuation',
+                    'string_verbatim',
+                    'string_escape_character',
+                    'class_name',
+                    'delegate_name',
+                    'enum_name',
+                    'interface_name',
+                    'module_name',
+                    'struct_name',
+                    'type_parameter_name',
+                    'field_name',
+                    'enum_member_name',
+                    'constant_name',
+                    'local_name',
+                    'parameter_name',
+                    'method_name',
+                    'extension_method_name',
+                    'property_name',
+                    'event_name',
+                    'namespace_name',
+                    'label_name',
+                    'xml_doc_comment_attribute_name',
+                    'xml_doc_comment_attribute_quotes',
+                    'xml_doc_comment_attribute_value',
+                    'xml_doc_comment_cdata_section',
+                    'xml_doc_comment_comment',
+                    'xml_doc_comment_delimiter',
+                    'xml_doc_comment_entity_reference',
+                    'xml_doc_comment_name',
+                    'xml_doc_comment_processing_instruction',
+                    'xml_doc_comment_text',
+                    'xml_literal_attribute_name',
+                    'xml_literal_attribute_quotes',
+                    'xml_literal_attribute_value',
+                    'xml_literal_cdata_section',
+                    'xml_literal_comment',
+                    'xml_literal_delimiter',
+                    'xml_literal_embedded_expression',
+                    'xml_literal_entity_reference',
+                    'xml_literal_name',
+                    'xml_literal_processing_instruction',
+                    'xml_literal_text',
+                    'regex_comment',
+                    'regex_character_class',
+                    'regex_anchor',
+                    'regex_quantifier',
+                    'regex_grouping',
+                    'regex_alternation',
+                    'regex_text',
+                    'regex_self_escaped_character',
+                    'regex_other_escape',
+                },
+            },
+        }
     end,
     handlers = {
         ['textDocument/definition'] = require('omnisharp_extended').handler,
