1972d19b13f4c640727a8f58de12b62742f7b5f8
Chloe-H <Chloe-H@users.noreply.github.com>
2023-09-08 22:27:24 -0500

    [reorg-rhel] Quit neovim, print message
    Add the last little thingy to the thingy

--- manual-backups/setup/neovim/linux/neovim-install.sh
+++ manual-backups/setup/neovim/linux/neovim-install.sh
@@ -117,4 +117,6 @@ fi
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim +PlugInstall
+nvim +PlugInstall +qall
+
+echo "Neovim setup complete. You may need to restart your terminal."
