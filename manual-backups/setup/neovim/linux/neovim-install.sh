670c7df89da406bd52b88344a292865e1731a807
Chloe-H <Chloe-H@users.noreply.github.com>
2023-09-11 14:53:26 -0500

    [reorg-rhel] Fix RHEL universal ctags install
    Comment out the example flag

--- manual-backups/setup/neovim/linux/neovim-install.sh
+++ manual-backups/setup/neovim/linux/neovim-install.sh
@@ -100,7 +100,7 @@ if [ -f /etc/os-release ]; then
             git clone https://github.com/universal-ctags/ctags.git
             cd ctags
             ./autogen.sh
-            ./configure --prefix=/where/you/want # defaults to /usr/local
+            ./configure # --prefix=/where/you/want # defaults to /usr/local
             make
             make install # may require extra privileges depending on where to install
             cd ${setup_dir}
