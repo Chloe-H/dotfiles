ab4899c412ba86bd074363c92e97e282bc80c8f1
Chloe-H <Chloe-H@users.noreply.github.com>
2023-10-05 11:35:10 -0500

    DRY telescope mappings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -325,41 +325,27 @@ vim.g.fzf_action = {
 -- Plugin settings: telescope.nvim
 local telescope = require('telescope')
 local ts_actions = require('telescope.actions')
+local ts_mappings = {
+    -- Send selected/all results to quickfix list and open it
+    ['<C-q>'] = ts_actions.smart_send_to_qflist + ts_actions.open_qflist,
+    -- Add selected/all results to quickfix list and open it
+    ['<M-q>'] = ts_actions.smart_add_to_qflist + ts_actions.open_qflist,
+
+    -- Preview scrolling
+    ['<C-f>'] = 'preview_scrolling_right',
+    ['<C-b>'] = 'preview_scrolling_left',
+    ['<C-k>'] = false, -- Disable
+
+    -- Results scrolling
+    ['<M-f>'] = 'results_scrolling_right',
+    ['<M-b>'] = 'results_scrolling_left',
+    ['<M-k>'] = false, -- Disable
+}
 telescope.setup({
     defaults = {
         mappings = {
-            i = {
-                -- Send selected/all results to quickfix list and open it
-                ['<C-q>'] = ts_actions.smart_send_to_qflist + ts_actions.open_qflist,
-                -- Add selected/all results to quickfix list and open it
-                ['<M-q>'] = ts_actions.smart_add_to_qflist + ts_actions.open_qflist,
-
-                -- Preview scrolling
-                ['<C-f>'] = 'preview_scrolling_right',
-                ['<C-b>'] = 'preview_scrolling_left',
-                ['<C-k>'] = false, -- Disable
-
-                -- Results scrolling
-                ['<M-f>'] = 'results_scrolling_right',
-                ['<M-b>'] = 'results_scrolling_left',
-                ['<M-k>'] = false, -- Disable
-            },
-            n = {
-                -- Send selected/all results to quickfix list and open it
-                ['<C-q>'] = ts_actions.smart_send_to_qflist + ts_actions.open_qflist,
-                -- Add selected/all results to quickfix list and open it
-                ['<M-q>'] = ts_actions.smart_add_to_qflist + ts_actions.open_qflist,
-
-                -- Preview scrolling
-                ['<C-f>'] = 'preview_scrolling_right',
-                ['<C-b>'] = 'preview_scrolling_left',
-                ['<C-k>'] = false,
-
-                -- Results scrolling
-                ['<M-f>'] = 'results_scrolling_right',
-                ['<M-b>'] = 'results_scrolling_left',
-                ['<M-k>'] = false, -- Disable
-            },
+            i = ts_mappings,
+            n = ts_mappings,
         },
     },
     pickers = {
