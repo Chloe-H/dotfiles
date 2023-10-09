c34e2266cae7989546bbcd468f981650e2586fb0
Chloe-H <Chloe-H@users.noreply.github.com>
2023-10-09 13:28:55 -0500

    Modify tabs in lualine
    Show [+] when the active buffer in a tab has unsaved modifications

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -285,6 +285,16 @@ require('lualine').setup({
                 mode = 2,
                 use_mode_colors = true,
                 max_length = vim.o.columns,
+
+                fmt = function(name, context)
+                    -- Show + if buffer is modified in tab
+                    local buflist = vim.fn.tabpagebuflist(context.tabnr)
+                    local winnr = vim.fn.tabpagewinnr(context.tabnr)
+                    local bufnr = buflist[winnr]
+                    local mod = vim.fn.getbufvar(bufnr, '&mod')
+
+                    return name .. (mod == 1 and ' +' or '')
+                end
             },
         },
         lualine_z = { require('auto-session.lib').current_session_name },
