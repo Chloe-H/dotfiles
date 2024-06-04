a5a2f4526d489c8d5581e9e79ab88ec8f4b8cc42
Chloe-H <Chloe-H@users.noreply.github.com>
2024-06-04 10:44:01 -0500

    Add script for exporting Habitica tasks

---
+++ manual-backups/scripts/curl-habitica-task.sh
@@ -0,0 +1,19 @@
+user_id=""
+auth_token=""
+task_id=""
+output_file=""
+default_output_file="./exported-task.json"
+
+read -p "User ID: " user_id
+read -p "API Token: " auth_token
+read -p "Task ID: " task_id
+read -p "Output file (${default_output_file}): " output_file
+
+output_file="${output_file:-${default_output_file}}"
+
+curl \
+    -H "x-api-user: ${user_id}" \
+    -H "x-api-key: ${auth_token}" \
+    --output ${output_file} \
+    https://habitica.com/api/v3/tasks/${task_id} \
+&& printf "\nDownloaded task ${task_id} to ${output_file}."
